4e197dc7b55cab75eb4422aea3ab7b49d334a3d3
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2025-02-25 23:59:19 +0200

    Update MainTable.js

--- components/MainTable.js
+++ components/MainTable.js
@@ -25,7 +25,6 @@ export default function DataTables() {
         }
 
         const fetchedData = await res.json();
-        console.log("Fetched Data:", fetchedData);
 
         if (Array.isArray(fetchedData)) {
           setData(fetchedData);
