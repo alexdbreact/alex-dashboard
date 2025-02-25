7981452a6dd071fc049721d1b114d7edbbccd278
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2025-02-26 00:02:44 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -31,14 +31,14 @@ const Footer = () => {
 
   </Link>
  
-  <Link href={"../servey"} className="  focus:border-blue-900 focus:outline-none  bg-teal-200 text-teal-900 focus:bg-blue-700 focus:text-slate-100 disabled">
+  <Link href={"../"} className="  focus:border-blue-900 focus:outline-none  bg-teal-200 text-teal-900 focus:bg-blue-700 focus:text-slate-100 disabled">
   <FaChartBar fontSize="1.5em"/>
 
     <span className="btm-nav-label text-center text-xm">Statistics & Evaluation</span>
    
   </Link>
   
-  <Link href={"../reports"} className="  focus:border-blue-900 focus:outline-none  bg-rose-200 text-rose-900 focus:bg-blue-700 focus:text-slate-100 disabled">
+  <Link href={"../"} className="  focus:border-blue-900 focus:outline-none  bg-rose-200 text-rose-900 focus:bg-blue-700 focus:text-slate-100 disabled">
   <MdScreenSearchDesktop  fontSize="1.5em"/>
 
     <span  className="btm-nav-label text-center text-xm ">E-Signature </span>
